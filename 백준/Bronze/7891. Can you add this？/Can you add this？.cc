54fb1dbdbeb9561a14c46fa622f2fc9e9eb82399
wavevening <161945656+wavevening@users.noreply.github.com>
2024-03-31 23:02:09 +0900

    [Bronze V] Title: Can you add this?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main() {
+	int co;
+	cin >> co;
+	for (int i = 0; i < co; i++) {
+		long long N, M;
+		cin >> N >> M;
+		cout << N + M << endl;
+	}
+
+	return 0;
+}
